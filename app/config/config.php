821913d9fc7655ea196c18cebbab3df21b555dd2
Ignacio Nevarez Zuñiga <55265068+IgnacioNevarez@users.noreply.github.com>
2019-12-25 11:33:21 -0600

    Enhanced MVC structure
    I improved the existing structure to adopt MVC with PHP, although it's still a work in progress.

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://192.168.64.2/gallery');
+define('SITENAME', 'Gallery')
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+  require_once('../app/requireFiles.php');
+  $init = new Core;
+?>
